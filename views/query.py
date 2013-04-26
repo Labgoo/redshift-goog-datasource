b66f94a2a387f9e9940a973cb29e2f631a4786e6
Shay Erlichmen <erlichmen@gmail.com>
2013-04-26 15:43:21 +0300

    add NoneType type mapping

--- views/query.py
+++ views/query.py
@@ -54,7 +54,7 @@ def query_db(connection, query,  meta_vars, vars):
 
     data = [dict((column, value) for column, value in row.items()) for row in rows]
 
-    type_convert = {"unicode": "string", "string": "string", "long": "number", "int": "number", "datetime": "datetime", "float": "number"}
+    type_convert = {"NoneType": "string", "unicode": "string", "string": "string", "long": "number", "int": "number", "datetime": "datetime", "float": "number"}
     if len(data) > 0:
         description = dict([(name, (type_convert[type(value).__name__], name)) for name, value in data[0].iteritems()])
     else:
