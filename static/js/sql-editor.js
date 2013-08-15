ea30562d2217d637e886c0297394a6423e36983c
Shay Erlichmen <erlichmen@gmail.com>
2013-08-15 17:25:03 +0300

    Revert "reverse order rows"
    This reverts commit b644aaf907ffd142f91b2dd4f8c81f3ea86e38ac.

--- static/js/sql-editor.js
+++ static/js/sql-editor.js
@@ -104,7 +104,7 @@ jQuery(function () {
             var aaData = [];
             var rows = data.K || data.J;
             if (rows) {
-                for  (var i=rows.length-1;i>=0;i--) {
+                for  (var i=0;i<rows.length;i++) {
                     var row = $.map(rows[i].c, function(val) {
                        return val.v;
                     });
